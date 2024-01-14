11484072b24037f03b6ea37ee33f5639c82deee4
axvg <91773368+axvg@users.noreply.github.com>
2024-01-13 20:37:34 -0500

    id

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.ionic.tuts',
   appName: 'tuts-ionic',
   webDir: 'www',
   server: {
